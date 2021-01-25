437bedcc7ec7e24a868719818a1a30d56324a7fa
Yaakov Abbou <jacobabbou@outlook.com>
2021-01-25 13:55:26 -0500

    Fix contact form error fields

--- js/validation.js
+++ js/validation.js
@@ -1,65 +1,93 @@
 $(document).ready(function () {
 	var isValid = true;
-	
-	$("#book_form").submit(function (event) {
-		event.preventDefault();
 
-		checkText('#title'); //Only letters, numbers, and white space allowed.
-		checkText('#prof');  //Only letters and white space allowed.
-		checkText('#major'); //Four capital letters, please.
-		checkNumber("#isbn"); //Ten digits, please.
+	var formId = $('form')[0].id;
+	if (formId == 'contact_form') {
+		validateContactForm();
+	}
+	if (formId == 'book_form') {
+		validateBookForm();
+	}
 
-		if (isValid == false) {
+	function validateBookForm() {
+		$("#book_form").submit(function (event) {
 			event.preventDefault();
-			$("#title").focus();
-		} else {
-			var count = 0;
-			var prof = $('#prof').val();
-			console.log(this.$parent.books);
-			this.$parent.books.forEach(book => {
-				if (book.professor == prof) {
-					count++;
-				}
-			});
-			alert(`Fun fact: ${count * 100 / books.length} of the books in our system are used by ${prof}!`);
-			this.submit();
-		}
-	});
+
+			checkText('#title'); //Only letters, numbers, and white space allowed.
+			checkText('#prof');  //Only letters and white space allowed.
+			checkText('#major'); //Four capital letters, please.
+			checkNumber("#isbn"); //Ten digits, please.
+
+			if (isValid == false) {
+				event.preventDefault();
+				$("#title").focus();
+			} else {
+				var count = 0;
+				var prof = $('#prof').val();
+				console.log(this.$parent.books); //does not yet access vue data()
+				this.$parent.books.forEach(book => {
+					if (book.professor == prof) {
+						count++;
+					}
+				});
+				alert(`Fun fact: ${count * 100 / books.length} of the books in our system are used by ${prof}!`);
+				this.submit();
+			}
+		});
+	}
+
+	function validateContactForm() {
+		$("#contact_form").submit(function (event) {
+			event.preventDefault();
+
+			checkText('#nameEntry');
+			checkEmail();
+			// checkText('#msgEntry');
+
+			if (isValid == false) {
+				event.preventDefault();
+				$("#nameEntry").focus();
+			} else {
+				this.submit();
+			}
+		});
+	}
 
 	function checkText(id) {
 		var input = $(id).val().trim();
-
+		var errorElement = $(id).siblings('span');
 		if (input == '' || ! /[A-Za-z]+/.test(input)) {
 			isValid = false;
-			$(id).siblings('span').text('Please enter letters only.').show();
+			errorElement.text('Please enter letters only.').show();
 		} else {
 			isValid = true;
-			$(id).next().text('').hide();
+			errorElement.text('').hide();
 		}
 	}
 
 	function checkNumber(id) {
 		var input = $(id).val().trim();
+		var errorElement = $(id).siblings('span');
 
 		if (input.length == 0 || ! /\d+/.test(input)) {
 			isValid = false;
-			$(id).next().text('Please enter numbers only.').show();
+			errorElement.text('Please enter numbers only.').show();
 		} else if (input.length < 10) {
 			isValid = false;
-			$(id).next().text('Please enter ten digits.').show();
+			errorElement.text('Please enter ten digits.').show();
 		}
 		else {
 			isValid = true;
-			$(id).next().text('').hide();
+			errorElement.text('').hide();
 		}
 	}
 
 	function checkEmail() {
-		var email = $("#email").val().trim();
+		var email = $("#emailEntry").val().trim();
 
 		if (email === "" || ! /^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/.test(email)) {
 			isValid = false;
-			email.next().text("Please enter a valid email.");
+			$("#emailEntry").next().text("Please enter a valid email.");
 		}
 	}
 });
