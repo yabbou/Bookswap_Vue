5e57fc6104aaa55ba0c4ba7abfff30cbfc31fde8
Yaakov Abbou <jacobabbou@outlook.com>
2021-01-27 19:07:49 -0500

    Set toolip at bottom

--- js/search.js
+++ js/search.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
 	$('[data-toggle="tooltip"]').mouseenter(
 		function () {
 			priceSearch(this.id);
-			setTooltipPrices(this.id);
 		}
 	);
 
@@ -17,7 +16,6 @@ $(document).ready(function () {
 			url: "https://www.googleapis.com/books/v1/volumes?q=" + id,
 			dataType: 'json',
 			method: 'GET',
-			timeout: 0,
 			success: function (data) {
 				var sellables = [];
 				data.items.forEach(book => {
@@ -26,34 +24,31 @@ $(document).ready(function () {
 					}
 				});
 
-				// var lp = 'Not available';
 				var rp = "";
 				var c = "";
 				if (sellables.length > 0) {
- 					var saleInfo = sellables[0].saleInfo;
+					var saleInfo = sellables[0].saleInfo;
 					if (saleInfo.saleability == "FOR_RENTAL_ONLY") {
 						rp = saleInfo.offers[0].retailPrice.amountInMicros / Math.pow(10, 6);
 						c = saleInfo.offers[0].retailPrice.currencyCode;
 					} else {
-						// lp = saleInfo.listPrice.amount;
 						rp = saleInfo.retailPrice.amount;
 						c = saleInfo.retailPrice.currencyCode;
 					}
-	
+
 					firstSellable.title = sellables[0].volumeInfo.title;
 					firstSellable.retailPrice = rp;
 					firstSellable.currency = c;
 				}
 				console.info(firstSellable.title + " " + rp);
+				setTooltipPrices(id);
 			}
 		});
 	}
 
 	function setTooltipPrices(id) {
-		var prices =
-			// `List price: ${firstSellable.listPrice} ${firstSellable.currency}\r 
-			`Retail price: ${firstSellable.retailPrice} ${firstSellable.currency}`;
-		$('svg#' + id).tooltip({ title: prices, placement: "top" });
+		var prices = `Retail price: ${firstSellable.retailPrice} ${firstSellable.currency}`;
+		$('a#' + id).tooltip({ title: prices, placement: "auto bottom" });
 	}
 
 });
